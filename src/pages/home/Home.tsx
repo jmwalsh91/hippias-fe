5f38279ed061c386c4119b782aa1e630968f95a8
jmwalsh91 <jordan@jmwalsh.dev>
2024-03-08 23:57:55 -0500

    landing animation

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,30 @@
+import { TypewriterEffect } from "@/components/ace/TypewriterEffect";
 import React from "react";
 
 type Props = {};
 
+const titleArray = [
+  { text: "Make", className: "text-lime-300 text-7xl" },
+  { text: "Curriculums", className: "text-cyan-300 text-7xl underline" },
+  { text: "Easier", className: "text-yellow-300 text-7xl" },
+  { text: "With", className: "text-red-300 text-7xl" },
+  { text: "Hippias", className: "text-violet-300 text-7xl italic" },
+];
 export default function Home({}: Props) {
-  return <div>Home</div>;
+  return (
+    <div className="block">
+      <div className="min-h-screen flex justify-center align-center">
+        <div className="self-center text-end">
+          <TypewriterEffect words={titleArray} />
+        </div>
+      </div>
+        <div className="min-h-screen">
+          <h1 className="text-4xl text-zinc-300">Welcome to Hippias</h1>
+          <p className="text-xl  text-zinc-300">
+            Hippias is a platform that allows you to create and share
+            curriculums with ease.
+          </p>
+        </div>
+    </div>
+  );
 }
